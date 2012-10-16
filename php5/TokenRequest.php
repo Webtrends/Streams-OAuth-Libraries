ce5192519282b90bfcc249f0491f2fa36aff09cd
seanm <sean.mcnamara@webtrends.com>
2012-10-16 11:08:51 -0600

    fixes to TokenRequest

--- php5/TokenRequest.php
+++ php5/TokenRequest.php
@@ -4,7 +4,7 @@ namespace Webtrends\Client\Authentication;
 
 class TokenRequest
 {
-	public $audience = "sauth.webtrends.com";
+	public $audience = "auth.webtrends.com";
 	public $scope = "sapi.webtrends.com";
 	public $authUrl = "sauth.webtrends.com/v1/token";
 	public $expiration;
@@ -14,7 +14,7 @@ class TokenRequest
 	
 	function __construct($clientId, $clientSecret)
 	{
-		$expiration = time() + 30*60;
+		$this->expiration = time() + 30*60;
 		$this->clientId = $clientId;
 		$this->clientSecret = $clientSecret;
 	}
@@ -22,19 +22,20 @@ class TokenRequest
 	function execute()
 	{
 		$encodedHeader = base64_encode(json_encode(array("typ"=>"JWT","alg"=>"HS256")));
-		$encodedClaims = base64_encode(json_encode(array("iss"=>$this->clientId,"prn"=>$this->clientId,"aud"=>$this->aduience,"exp"=>$this->expiration,"scope"=>$this->scope)));
+		$encodedClaims = base64_encode(json_encode(array("iss"=>$this->clientId,"prn"=>$this->clientId,"aud"=>$this->audience,"exp"=>$this->expiration,"scope"=>$this->scope)));
 		$sig = base64_encode(hash_hmac("sha256", "$encodedHeader.$encodedClaims", $this->clientSecret, true));
 		$fields = array("client_id"=>$this->clientId,"client_assertion"=>"$encodedHeader.$encodedClaims.$sig");
 		
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $this->audience);
+		curl_setopt($ch, CURLOPT_URL, $this->authUrl);
 		curl_setopt($ch, CURLOPT_POST, count($fields));
 		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($fields));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		$raw_result = curl_exec($ch);
+		$info = curl_getinfo($ch);
 		curl_close($ch);
-
-		if (!$raw_result)
+		
+		if ($info["http_code"] != 200 || !$raw_result)
 			throw new \Exception("Request error");
 		
 		$result = json_decode($raw_result, true);
--- php5/example.php
+++ php5/example.php
@@ -4,7 +4,7 @@ require_once "TokenRequest.php";
 use Webtrends\Client\Authentication\TokenRequest;
 
 try {
-	$tr = new TokenRequest("a","b");
+	$tr = new TokenRequest("195D066921AA4545A8E6230DFA667DCE","93AF483281EA454D9CEE790B5E92F106");
 	$token = $tr->execute();
 	print "Your token is: $token\n";
 } catch (Exception $e)
